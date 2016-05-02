e0edb292ddb00783ba87a04c20f2c088c1e7cf84
Mark Brewster <m.brewster@volcanic.co.uk>
2016-05-02 16:20:28 +0100

    class added and first tests

--- job_list.rb
+++ job_list.rb
@@ -1,4 +1,13 @@
 class JobList
+  attr_reader :jobs_str
+  private :jobs_str
 
+  def initialize(jobs_str)
+    @jobs_str = jobs_str
+  end
+  
+  def sorted_jobs
+    'a'
+  end
   
 end
--- spec/job_list_spec.rb
+++ spec/job_list_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
  
 describe JobList do
   before :each do
-      @job_list = JobList.new('a =>')
+    @job_list ||= JobList.new('a =>')
   end
   
   describe "#new" do
     it 'accepts a single string argument' do
-      @job_list.should be_an_instance_of JobList
+      expect(@job_list).to be_an_instance_of(JobList)
+    end
+  end
+  
+  describe "#sorted_jobs" do
+    it 'should be a sequence consisting of a single job a' do
+      expect(@job_list.sorted_jobs).to eql('a')
     end
   end
   
